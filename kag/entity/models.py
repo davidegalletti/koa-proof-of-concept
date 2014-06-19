1fcc89e73b366463cfd20a895f3b3067d81055ab
Davide Galletti <davide.galletti@gmail.com>
2014-06-19 16:22:07 +0200

    EntityTree

--- kag/entity/models.py
+++ kag/entity/models.py
@@ -5,9 +5,73 @@ class DBConnection(models.Model):
     connection_string = models.CharField(max_length=255L)
 
 
-class Entity(models.Model):
-    version = models.IntegerField(blank=True)
+
+class WorkflowEntity(models.Model):
+    '''
+    Abstract class
+    WorkflowEntity is a generic entity with a work-flow status; such a status is used also for minimal work-flow of entities that are either in "working" or "released" status
+    '''
+    workflow = models.ForeignKey('Workflow', null=True, blank=True, related_name = "+")
+    class Meta:
+        abstract = True
+    
+class Workflow(WorkflowEntity):
+    '''
+    Is a list of WorkflowMethods; the work-flow is abstract, its methods do not specify details of the operation but just the statuses
+    '''
     name = models.CharField(max_length=100L)
+    description = models.CharField(max_length=2000L, blank=True)
+  
+class WorkflowStatus(models.Model):
+    '''
+    TODO: We need to have some statuses that are available to any entity and some just to specific entities; how?
+    '''
+    name = models.CharField(max_length=100L)
+    workflow = models.ForeignKey(Workflow, null=True, blank=True)
+    description = models.CharField(max_length=2000L, blank=True)
+
+
+class WorkflowEntityInstance(models.Model):
+    '''
+    Abstract class
+    WorkflowEntityInstance
+    '''
+    current_status = models.ForeignKey(WorkflowStatus)
+    class Meta:
+        abstract = True
+    
+    
+class VersionableEntityInstance(models.Model):
+    '''
+    Abstract class
+    VersionableEntityInstance is ....
+    '''
+    version = models.IntegerField(blank=True)
+    class Meta:
+        abstract = True
+    
+class WorkflowMethod(models.Model):
+    '''
+    If there are no initial_statuses then this is a method which creates the entity
+    TODO: Can the final status be dynamically determined by the implementation?
+    '''
+    initial_statuses = models.ManyToManyField(WorkflowStatus, blank=True, related_name="+")
+    final_status = models.ForeignKey(WorkflowStatus, related_name="+")
+    workflow = models.ForeignKey(Workflow)
+
+    class Meta:
+        abstract = True
+
+
+
+class Entity(WorkflowEntity):
+    '''
+    Every entity has a work-flow; the basic one is the one that allows a method to create an instance
+    '''
+    #corresponds to the class name
+    name = models.CharField(max_length=100L)
+    version = models.IntegerField(blank=True)
+    app = models.CharField(max_length=100L)
     description = models.CharField(max_length=2000L, blank=True)
     table_name = models.CharField(max_length=255L, db_column='tableName', blank=True) 
     id_field = models.CharField(max_length=255L, db_column='idField', blank=True)
@@ -18,9 +82,32 @@ class Entity(models.Model):
     def __unicode__(self):
         return self.name + " (" + self.version + ")" 
 
+class AttributeType(models.Model):
+    name = models.CharField(max_length=255L, blank=True)
+    widgets = models.ManyToManyField('application.Widget', blank=True)
+
 class Attribute(models.Model):
     name = models.CharField(max_length=255L, blank=True)
     entity = models.ForeignKey('Entity', null=True, blank=True)
+    type = models.ForeignKey('AttributeType')
     def __unicode__(self):
         return self.entity.name + "." + self.name 
 
+class EntityTreeNode(models.Model):
+    entity = models.ForeignKey('Entity')
+    # attribute is blank for the entry point
+    attribute = models.CharField(max_length=255L, blank=True)
+    child_nodes = models.ManyToManyField('self', blank=True)
+   
+class EntityTree(models.Model):
+    '''
+    It is a tree that defines a set of entities on which we can perform a task; the tree has
+    an entry point which is a Node e.g. an entity; from an instance of an entity we can use
+    the "attribute" attribute from the corresponding EntityTreeNode to get the instances of
+    the entities related to each of the child_nodes. An EntityTree could, for example, tell
+    us what to export to xml/json, what to consider as a "VersionableMultiEntity" (e.g. an
+    instance of VersionableEntityInstance + an EntityTree where the entry_point points to that instance)
+    The name should describe its use
+    '''
+    name = models.CharField(max_length=200L)
+    entry_point = models.ForeignKey('EntityTreeNode')
