c99d29935b338475012af5189fa9e91025d49548
Davide Galletti <davide.galletti@gmail.com>
2014-06-19 16:22:07 +0200

    EntityTree

--- kag/application/models.py
+++ kag/application/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from entity.models import WorkflowMethod, WorkflowEntity, VersionableEntityInstance
 
-
-class Method(models.Model):
+class Method(WorkflowMethod):
+    '''
+    TODO: let it inherit from Entity.WorkflowMethod
+    '''
     name = models.CharField(max_length=255L, blank=True)
     description = models.TextField(blank=True)
     attributes = models.ManyToManyField('entity.Attribute', blank=True)
@@ -10,13 +13,21 @@ class Method(models.Model):
     script_postcondition = models.TextField(blank=True)
     script_premethod = models.TextField(blank=True)
     script_postmethod = models.TextField(blank=True)
+
     def __unicode__(self):
         return self.name
 
-class Application(models.Model):
+class Widget(models.Model):
+    '''
+    It is a django widget; TBC
+    '''
+    widgetname = models.CharField(max_length=255L, blank=True)
+
+class Application(WorkflowEntity, VersionableEntityInstance):
     name = models.CharField(max_length=255L, blank=True)
     description = models.TextField(blank=True)
     methods = models.ManyToManyField(Method)
+
     def __unicode__(self):
         return self.name
 
