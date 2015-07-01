08d039cac3eaa21adee7ed16df18641bb4b76487
Davide Galletti <davide.galletti@gmail.com>
2015-07-01 18:44:26 +0200

    small migration fix

--- kag/entity/migrations/ks_specific/0004_testonu.py
+++ kag/entity/migrations/ks_specific/0004_testonu.py
@@ -12,16 +12,16 @@ def forwards_func(apps, schema_editor):
     orgs = Organization.objects.using(db_alias).bulk_create([
         Organization(pk=2, name="United Nations", URIInstance="http://testonuks.thekoa.org/entity/Organization/1", description="Test United Nations"),
     ])
-    test_interni_it_org = orgs[0]
+    test_onu_org = orgs[0]
 
     root_ks = KnowledgeServer.objects.get(pk=1)
     root_ks.this_ks = False
     root_ks.save()
     
     KSs = KnowledgeServer.objects.using(db_alias).bulk_create([
-        KnowledgeServer(pk=2, name="Test interni.it KS", scheme="http", netloc="testinterniitks.thekoa.org", URIInstance="http://testonuks.thekoa.org/entity/KnowledgeServer/2", description="Test interni.it KS", organization=test_interni_it_org, this_ks=True),
+        KnowledgeServer(pk=2, name="Test ONU KS", scheme="http", netloc="testonuks.thekoa.org", URIInstance="http://testonuks.thekoa.org/entity/KnowledgeServer/2", description="Test ONU KS", organization=test_onu_org, this_ks=True),
     ])
-    test_interni_it_org_ks = KSs[0]
+    test_onu_org_ks = KSs[0]
     
 
 
