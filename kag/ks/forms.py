98975eb989c581a14bfaca1498d374676dbb4707
Davide Galletti <davide.galletti@gmail.com>
2015-08-27 18:42:13 +0200

    minor

--- kag/ks/forms.py
+++ kag/ks/forms.py
@@ -4,5 +4,5 @@
 from django import forms
 
 class ExploreOtherKSForm(forms.Form):
-    ks_complete_url = forms.CharField(required=True, label="KS complete URL", help_text='<protocol><domain><port><path> e.g. http://licensedemo.thekoa.org')
+    ks_complete_url = forms.CharField(required=True, label="KS complete URL", help_text='<protocol><domain><port><path> try: http://licensedemo.thekoa.org')
 
