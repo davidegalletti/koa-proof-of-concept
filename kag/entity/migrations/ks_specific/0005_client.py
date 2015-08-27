ea2edb967ace3f61e673ef4c3e90060955b31a26
Davide Galletti <davide.galletti@gmail.com>
2015-08-27 09:40:10 +0200

    demo stuff

---
+++ kag/entity/migrations/ks_specific/0005_client.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from entity.models import Organization, KnowledgeServer, EntityInstance, EntityStructure, SimpleEntity, EntityStructureNode
+
+def forwards_func(apps, schema_editor):
+    test_client_org = Organization();test_client_org.name="A demo Organization";test_client_org.website='http://demo.example.com';test_client_org.description="This is just a demo Organization.";
+    test_client_org.save(using='default')
+    id_on_default_db = test_client_org.id
+    test_client_org.id = None
+    test_client_org.save(using='ksm')client
+    m_test_client_org = test_client_org
+    test_client_org = Organization.objects.get(pk=id_on_default_db)
+    
+    root_ks = KnowledgeServer.this_knowledge_server('default')
+    root_ks.this_ks = False
+    root_ks.save()
+    root_ks = KnowledgeServer.this_knowledge_server()
+    root_ks.this_ks = False
+    root_ks.save()
+    
+    m_test_client_org_ks = KnowledgeServer(name="A demo OKS used as a client.", scheme="http", netloc="client.thekoa.org", description="Just a test.", organization=test_client_org, this_ks=True)
+    m_test_client_org_ks.save(using='ksm')
+    test_client_org_ks = m_test_client_org_ks
+    test_client_org_ks.id = None
+    test_client_org_ks.URIInstance = ""
+    test_client_org_ks.save(using='default')
+    
+    # m_test_client_org and test_client_org have the wrong URIInstance because they where created before their Knowledge Server
+    # I fix this:
+    m_test_client_org.URIInstance = ""
+    m_test_client_org.save()
+    test_client_org.URIInstance = ""
+    test_client_org.save()
+    
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('entity', '0004_common_to_other_ks'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            forwards_func,
+        ),
+    ]
+
--- kag/static/js/oks.js
+++ kag/static/js/oks.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     var today = new Date();
     var m = 60 -today.getMinutes();
-    document.getElementById('time').innerHTML = "This demo will be reset every hour. " + m + " minutes to go.";
+    document.getElementById('time').innerHTML = "This demo will be reset every hour: " + m + " minutes to go.";
     $(function() {
       $( document ).tooltip();
     });
